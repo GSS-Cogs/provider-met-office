ed26ea41d54bbfcfca35bff0dd9136c39b7a5a4b
bailed22 <95625808+bailed22@users.noreply.github.com>
2023-04-18 15:50:18 +0100

    met-office-regional-average-climate-observations-uk-annual-mean-temperature-2022

---
+++ met-office-regional-average-climate-observations-uk-annual-mean-temperature-2022/wrangling.py
@@ -0,0 +1,32 @@
+import click
+import pandas as pd
+import numpy as np
+import math
+from pathlib import Path
+
+@click.command()
+@click.argument("input", type=click.Path(exists=True, path_type=Path))
+@click.option("--output", default=Path("./output.csv"), type=click.Path(path_type=Path))
+def wrangle(input: Path(), output: Path()) -> None:
+
+    df = pd.read_csv("regional-average-climate-observations-uk-annual-mean-temperature.csv")
+
+    df['Measure Type'] = df.apply(
+    lambda x: 'Annual Mean Temperature (Trend)' if 'trend' in x['Geography'] 
+                else 'Annual Mean Temperature', axis=1)
+
+    df['Geography'] = df['Geography'].apply(lambda x: 'K02000001' if 'uk' in x
+                                        else ('N92000002' if 'northern-ireland' in x
+                                              else ('E92000001' if 'england' in x
+                                                    else ('S92000003' if 'scotland' in x
+                                                          else ('W92000004' if 'wales' in x else x)))))
+    
+    df = df.rename(columns={'Year': 'Period', 'Measure Type': 'Measure', 'Value': 'Observation'})
+
+    df = df[['Period', 'Geography', 'Measure', 'Observation']] 
+
+    df.to_csv(output, index=False)
+    return
+
+if __name__ == "__main__":
+    wrangle()
